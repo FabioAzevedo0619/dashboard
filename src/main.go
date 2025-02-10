122d51d1ce38534475a00bbed97254eafa8cef73
Fabio <fabio.am.azevedo@proton.me>
2025-02-10 21:01:40 +0000

    Implemented styling and page structure

--- src/main.go
+++ src/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"html/template"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 )
 
 type Service struct {
@@ -22,17 +25,34 @@ type Dashboard struct {
     Groups []Group `json:"groups"`
 }
 
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-    tmpl := template.Must(template.ParseFiles("templates/index.html"))
-    data := struct {
-        Title   string
-        Message string
-    }{
-        Title:   "Hello Page",
-        Message: "Hello, World!",
+func loadConfig() (Dashboard, error) {
+    var dashboard Dashboard
+    configFile, err := os.Open("config/config.json")
+    if err != nil {
+        return dashboard, err
+    }
+    defer configFile.Close()
+
+    byteValue, err := ioutil.ReadAll(configFile)
+    if err != nil {
+        return dashboard, err
     }
 
-    if err := tmpl.Execute(w, data); err != nil {
+    err = json.Unmarshal(byteValue, &dashboard)
+    return dashboard, err
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+    tmpl := template.Must(template.ParseFiles("templates/index.html"))
+
+    dashboard, err := loadConfig()
+    if err != nil {
+        log.Printf("Error loading config: %v", err)
+        http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+        return
+    }
+
+    if err := tmpl.Execute(w, dashboard); err != nil {
         log.Printf("Error executing template: %v", err)
     }
 }
